98fd3bc5fdb15ab05df26032fe876c657c177be0
Mazen <mmeezzoo26@me.com>
2016-03-22 08:01:34 +0100

    generate Devise views

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -12,3 +12,10 @@ end
 Then(/^Show me the page$/) do
   save_and_open_page
 end
+
+Then(/^I should be on the "([^"]*)" page$/) do |page|
+  case page
+    when 'Sign up'
+      expect(current_path).to eq '/users/sign_up'
+  end
+end
